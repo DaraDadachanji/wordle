f3d44c3260c201bcd5f514bd4a671b0fd59a840b
dara.dadachanji <dara.dadachanji@irhythmtech.com>
2022-01-14 23:25:12 -0800

    split up game logic from CLI

--- src/simulate.py
+++ src/simulate.py
@@ -1,18 +1,16 @@
 
-import tkinter
 import pandas as pd
 import termcolor
 import random
-import enum
+import wordle
 
-guesses = pd.read_csv("src/guesses.txt", delimiter="\n")
 
 def simulate():
     answer = get_random_answer()
     while True:
         guess = get_guess()
-        if validate(guess):
-            hint = check(guess=guess, answer=answer)
+        if wordle.validate(guess):
+            hint = wordle.check(guess=guess, answer=answer)
             render(hint)
             if guess == answer:
                 break
@@ -30,78 +28,16 @@ def get_guess() -> str:
     return guess
 
 
-def validate(guess: str) -> bool:
-    return guess in guesses["word"].values
-
-class State(enum.Enum):
-    CORRECT = 2
-    PRESENT = 1
-    ABSENT = 0
-
-
-class Letter:
-    def __init__(self, letter: str) -> None:
-        self.rune = letter
-        self.state = State.ABSENT
-        self.accounted_for = False
-    
-    def __eq__(self, __o: object) -> bool:
-        if isinstance(__o, Letter):
-            return __o.rune == self.rune
-        elif isinstance(__o, str):
-            return __o == self.rune
-        else:
-            return False
-
-    def mark_correct(self):
-        self.state = State.CORRECT
-        self.accounted_for = True
-
-    def mark_present(self):
-        self.state = State.PRESENT
-        self.accounted_for = True
-
-
-
-class Word(list[Letter]):
-    def __init__(self, word: str) -> None:
-        letters: list[Letter] = [Letter(letter) for letter in word]
-        super(Word, self).__init__()
-        self.extend(letters)
-    
-    def contains(self, check: Letter) -> bool:
-        for letter in self:
-            if not letter.accounted_for:
-                if check == letter:
-                    letter.accounted_for = True
-                    return True
-
-
-
-def check(guess: str, answer: str) -> list[tkinter.Text]:
-    w_answer = Word(answer)
-    hint = Word(guess)
-    for i in range(5):
-        if hint[i] == answer[i]:
-            hint[i].mark_correct()
-            w_answer[i].accounted_for = True
-    for i in range(5):
-        if not hint[i].accounted_for:
-            if w_answer.contains(hint[i]):
-                hint[i].mark_present()
-    return hint
-
-
-def render(hint: Word):
+def render(hint: wordle.Word):
     highlights = {
-        State.CORRECT: "on_green",
-        State.PRESENT: "on_yellow",
-        State.ABSENT: "on_grey"
+        wordle.State.CORRECT: "on_green",
+        wordle.State.PRESENT: "on_yellow",
+        wordle.State.ABSENT: "on_grey"
     }
     text_colors = {
-        State.CORRECT: "grey",
-        State.PRESENT: "grey",
-        State.ABSENT: "white"
+        wordle.State.CORRECT: "grey",
+        wordle.State.PRESENT: "grey",
+        wordle.State.ABSENT: "white"
     }
     output = []
     for letter in hint:
